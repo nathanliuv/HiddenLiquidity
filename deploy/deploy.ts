5d400bae35955646a882f4042808d2cc0ed58cad
nathanliuv <gpwtixn5264@outlook.com>
2025-12-01 13:04:10 +0800

    fix deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,6 +2,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const UNISWAP_V2_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const GAS_LIMIT = 16_500_000;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
@@ -11,6 +12,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     from: deployer,
     args: [deployer],
     log: true,
+    gasLimit: GAS_LIMIT,
   });
 
   log(`cUSDC token deployed to ${cusdc.address}`);
@@ -19,11 +21,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     from: deployer,
     args: [cusdc.address, UNISWAP_V2_ROUTER],
     log: true,
+    gasLimit: GAS_LIMIT,
   });
 
   log(`Swap contract deployed to ${swap.address}`);
 
-  await execute("CUSDC", { from: deployer, log: true }, "setMinter", swap.address);
+  await execute("CUSDC", { from: deployer, log: true, gasLimit: GAS_LIMIT }, "setMinter", swap.address);
   log(`Set EthCUSDSwap as minter for cUSDC`);
 };
 
